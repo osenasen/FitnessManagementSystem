aa571562551dcd8a5bb38424326143189bb99ad7
Joshua Selbstaedt <joshua.selbstaedt@stud.fh-dortmund.de>
2024-06-13 18:59:27 +0200

    update ClientProfileView.fxml

--- src/main/java/fms/controller/ClientProfileController.java
+++ src/main/java/fms/controller/ClientProfileController.java
@@ -9,75 +9,51 @@ import java.io.IOException;
 import fms.model.ClientModel;
 
 public class ClientProfileController {
-  
+
   @FXML
   private Pane centerPane;
-  
+
   @FXML
-  private Label nameLabel;
-  
+  private Label clientNameLabel;
+
   @FXML
-  private Label healthInfoLabel;
-  
-  @FXML
-  private Label goalLabel;
-  
-  @FXML
-  private Label workoutLabel;
-  
-  @FXML
-  private Label dietLabel;
-  
-  @FXML
-  private Label generalInfoLabel;
-  
-  @FXML
-  private Label workoutDetailsLabel;
-  
-  @FXML
-  private Label dietDetailsLabel;
-  
+  private Label clientIdLabel;
+
   private ClientModel client;
-  
+
   public void setClient(ClientModel client) {
     this.client = client;
     displayClientData();
     showDashboardView();
   }
-  
+
   private void displayClientData() {
     if (client != null) {
-      nameLabel.setText(client.getName());
-      healthInfoLabel.setText(client.getHealthInfo());
-      goalLabel.setText(client.getGoal());
-      workoutLabel.setText(client.getWorkout());
-      dietLabel.setText(client.getDiet());
-      generalInfoLabel.setText("Health: " + client.getHealthInfo() + "\nGoal: " + client.getGoal());
-      workoutDetailsLabel.setText(client.getWorkout());
-      dietDetailsLabel.setText(client.getDiet());
+      clientNameLabel.setText(client.getName());
+      clientIdLabel.setText(String.valueOf(client.getId()));
     }
   }
-  
+
   @FXML
   private void showNutritionView() {
     loadView("/view/NutritionViewFragment.fxml");
   }
-  
+
   @FXML
   private void showExerciseView() {
     loadView("/view/ExerciseViewFragment.fxml");
   }
-  
+
   @FXML
   private void showDashboardView() {
     loadView("/view/DashboardViewFragment.fxml");
   }
-  
+
   @FXML
   private void showGeneralInfoView() {
     loadView("/view/GeneralInfoViewFragment.fxml");
   }
-  
+
   private void loadView(String fxmlFile) {
     try {
       FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));

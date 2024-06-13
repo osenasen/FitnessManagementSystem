5e66bda9e1ed68eae69aed69f80245878f298e21
Joshua Selbstaedt <joshua.selbstaedt@stud.fh-dortmund.de>
2024-06-13 18:53:14 +0200

    update ClientProfileView.fxml

--- src/main/java/fms/controller/NutritionController.java
+++ src/main/java/fms/controller/NutritionController.java
@@ -1,14 +1,5 @@
 package fms.controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class NutritionController {
-  
-  @FXML
-  private Label nutritionLabel;
-  
-  public void initialize() {
-    nutritionLabel.setText("Nutrition Info");
-  }
+    // Future implementation for Nutrition view
 }
